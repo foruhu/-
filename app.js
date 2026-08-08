a12a5028536c73ad48e3a05d1c11dd376488e0ce
foruhu <foruhu@gmail.com>
2026-08-08 11:08:18 +0900

    app.js を更新

--- app.js
+++ app.js
@@ -290,17 +290,6 @@ function exportJSON() {
   URL.revokeObjectURL(url);
 }
 
-/* モーダル背景 */
-.modal-overlay {
-  position: fixed;
-  top: 0; left: 0;
-  width: 100%; height: 100%;
-  background: rgba(0, 0, 0, 0.7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 10000;
-}
 
 /* モーダルコンテンツ */
 .modal-content {
